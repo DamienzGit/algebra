97ccdeed93b8e08719644e5cbb4e1b84bfa923c6
DamienzGit <damianomails@gmail.com>
2018-01-17 03:32:51 +0100

    init 2

--- modular-algebra.py
+++ modular-algebra.py
@@ -1,11 +1,22 @@
 from sympy.ntheory import isprime, factorint
 
+
 def phi(n):
-    if isprime(n):
-        return n-1
-    factors = factorint(n)
-    prod = 1
-    for factor, exp in factors.items():
-        prod *= factor**(exp-1) * (factor - 1)
+    if isprime(n): return n-1
+    factors, prod = factorint(n), 1
+    for factor, exp in factors.items(): prod *= factor**(exp-1) * (factor - 1)
     return prod
+
+def egcd(a, b):
+    if a == 0: return (b, 0, 1)
+    g, y, x = egcd(b%a,a)
+    return (g, x - (b//a) * y, y)
+
+def _modinv(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1: raise Exception('No modular inverse')
+    return x%m
+
+def invert(n, mod): return _modinv(n, mod)
+
 
